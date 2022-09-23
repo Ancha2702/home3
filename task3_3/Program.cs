d0e7b6a7351d4810ddbe9c0bd8919883bfb5f441
Anna <Ancha.ru@mail.ru>
2022-09-23 13:47:07 +0300

    Третье задание из дз3

---
+++ task3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+for (count =1; count <= n; count++)
+{
+    Console.Write(Math.Pow(count,3));
+    if (count != n)
+    {   
+    Console.Write(", ");
+    }
+}
+Console.WriteLine(" ");
